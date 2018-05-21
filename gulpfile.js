f92d2bda50ec06c2ae2f4f4255d4acf8fd84ed6c
BBoBBG <github@empirictechs.com>
2018-05-21 20:10:04 +0300

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,6 @@ const gulp = require('gulp');
 var bitcoreTasks = require('../bitcore-build-mue');
 
 bitcoreTasks('submodule');
+gulp.task('default', ['lint', 'test', 'browser', 'coverage']);
+
 gulp.task('test', ( console.log('It works!')));
